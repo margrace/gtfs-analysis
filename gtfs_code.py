710870668bcb5d12d7e95c041fe706e75ffb02ec
marco <marcogranierocelis@gmail.com>
2023-09-22 18:44:05 -0400

    Addition of a Python file to perform the analysis

---
+++ gtfs_code.py
@@ -0,0 +1,10 @@
+import os
+import zipfile
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+root = os.path.dirname(os.path.abspath(__file__))
+
+print(root) 
